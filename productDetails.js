8d5a7be92231433b9278a9076f3919166581fa8f
Chhaya7817 <chhaya.chaudhary.visit.me.in91@gmail.com>
2022-11-21 19:21:19 +0530

    login

--- productDetails.js
+++ productDetails.js
@@ -2,10 +2,11 @@ const Bname=document.getElementsByClassName("name");
 window.onload=function(){
 for(let i=0;i<Bname.length;i++)
     {
-    console.log(Bname[i]);
+    // console.log(Bname[i]);
     Bname[i].onclick=function(){
-        window.location.href="./productDetails.html";
-        var k=document.getElementById('image');
+        window.location.href="./productDetails.html(";
+        var k=document.getElementsByClassName('');
+
         console.log(k);
         
         
