67862148ebd612b5c8df38fcde2f53f19a5340c7
Chhaya7817 <chhaya.chaudhary.visit.me.in91@gmail.com>
2022-11-22 23:31:38 +0530

    final

--- script.js
+++ script.js
@@ -1,41 +1,47 @@
 
 window.onload=function(){
-    document.getElementById('cart').onclick=function()
-    {
-    // console.log(logo);
-    window.location.href="./cart.html";
-    }
     let loginForm = document.querySelector('.login-form-container');
 
     document.querySelector('#login').onclick = () =>{
     loginForm.classList.toggle('active');
     }
+    document.querySelector('#login1').onclick = () =>{
+        loginForm.classList.toggle('active');
+        }
 
     document.querySelector('#close-login-btn').onclick = () =>{
     loginForm.classList.remove('active');
     }
+
+    //cart
+    const cart = document.getElementById("cartlist");
+    document.getElementById('cart1').onclick=function()
+    {
+    // console.log(logo);
+    cart.style.display = "block";
+    }
     
+    
+    const targetDiv = document.getElementById("menu");
+const btn = document.getElementById("list");
 
-    //Product details 
-    // var Pdetails=document.querySelector('.Pdetails')
-    // const Bname=document.getElementsByClassName("name");
-    // for(let i=0;i<Bname.length;i++)
-    // {
-    // // console.log(Bname[i]);
-    // Bname[i].onclick=function(){
-    //     // window.location.href="./productDetails.html";
-    //     // getElementsByClassName('name')[0];
-    //     Bname[i].onclick=function(){
-    //         loginForm.classList.toggle('active');
-    //         document.querySelector('.Pname').innerHTML= console.log(document.getElementsByClassName('books')[0].getElementsByClassName('name')[0]);
-    //         Pdetails.classList.remove('active');
-    //     }
-        
-        
-    // }
+btn.onclick = function () {
+  
+    targetDiv.style.display = "block";
+  
+};
 
-    // console.log(document.getElementsByClassName('books')[0].getElementsByClassName('name')[0]);
-    // }
+// const cart = document.getElementById("cartlist");
+// const cartbtn = document.getElementById("cart1");
+
+// cartbtn.onclick = function () {
+  
+//     cart.style.display = "block";
+  
+// };
+
+
+    
 
 
 }
